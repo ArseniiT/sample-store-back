c4d64e2e80ba56df83ecc0d286e352f7e5045c5b
arseniismpx <arseniit@gmail.com>
2023-10-27 18:16:03 +0200

    Updated User entity and User repository Added User table migration
    Swagger UI added and works well
    Versions compatibility: jdk-21, Spring Boot (3.1.4), springdoc-openapi-starter-webmvc-ui:2.2.0
    https://springdoc.org/#getting-started
    https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui/2.2.0

--- src/main/java/com/example/sample3/Sample3Application.java
+++ src/main/java/com/example/sample3/Sample3Application.java
@@ -2,15 +2,19 @@ package com.example.sample3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Sample3Application {
 
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Sample3Application.class, args);
+
+
+
+
 	}
 
 }
